831b5d3a08919e8c9e2733d42f7872777f214467
shittydesigns <70614352+shittydesigns@users.noreply.github.com>
2021-04-29 22:26:47 +1000

    Update whale.cpp

--- whale.cpp
+++ whale.cpp
@@ -24,29 +24,34 @@ class [[eosio::contract]] whale : public contract {
       require_recipient( from );
       require_recipient( to );
 }
+
       [[eosio::action]]
+//      [[eosio::on_notify("eosio.token::transfer")]]
 //Make sure you include eosio.code in your active permission!!!
 //Refer to http://github.com/EOSIO/eosio.cdt/issues/250 for asset information
 //Loop is in uint32.
 //[[eosio::action]] OR [[eosio::on_notify("Whatever token::Whatever action")]] you want to leech on
 
 //ONLY REQUIRED WHEN IT'S AN ACTION
-//      void claim() {
+      void claim(const uint32_t loops) {
 //      require_auth( _self );
 
-       void claim( const name from,
-                   const name to,
-                   const asset quantity,
-                   const std::string memo ) {
+//       void claim( const name from,
+//                   const name to,
+//                   const asset quantity,
+//                   const std::string memo ) {
 
 
 //Don't fuck yourself up in using on_notify!
 //Only USE IT WHEN YOU HAVE NAME FROM AND TO!!! OR AN ERROR WOULD BE THROWN!
-      if (to != get_self() || from == get_self()) return;
+
+//      if (to != get_self() || from == get_self()) return;
+//      if (from == "eosiopowcoin"_n) return;
 
 //I don't think the rest need explination, i <= x is the amount of time you want it
       int count = 1;
-   while( count < 100 ) {
+//   while( count < 50 ) {
+   while( count < loops ) {
       action {
                       permission_level{get_self(), "active"_n},
                       "eosio.token"_n, "transfer"_n,
