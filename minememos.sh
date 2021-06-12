00eaef4b81dacb809f9ba92baf299ed4ee1a3ed6
shittydesigns <admin@thecattoken.com>
2021-06-12 12:20:56 +1000

    Add files via upload

---
+++ minememos.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+while true
+do
+cleos -u https://eos.newdex.one:443 push action iamawhale.m claim '["90"]' -p accountmemos@claim --expiration 1800 --max-cpu-usage-ms 25
+read -t 20 -p "I am going to wait for 20 seconds only ..."
+done
